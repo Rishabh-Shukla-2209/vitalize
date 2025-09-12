0f62758c5e5cf4053994f7c0ab671452222aad23
Rishabh Shukla <rishabhshukla7863@gmail.com>
2025-09-12 13:06:13 +0530

    Finalize Folder Structure

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,8 @@
+import Logo from '@/ui/components/icons/Logo'
+export default function Home() {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+}
---
+++ src/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Spline_Sans } from 'next/font/google';
+ 
+export const spline_sans = Spline_Sans({ subsets: ['latin'], weight: ["400", "500", "700"] });
